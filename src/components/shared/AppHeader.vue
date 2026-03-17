3464af2d611123be0c08c990b4987b4d39a8dae3
capricornus44 <antonzavalniuk@gmail.com>
2026-03-17 20:03:26 +0100

    feat: Implement a comprehensive DropdownMenu component suite, a ThemeToggle, and an AppHeader.

---
+++ src/components/shared/AppHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Github } from 'lucide-vue-next'
+import { Button } from '@/components/ui/button'
+import ThemeToggle from './ThemeToggle.vue'
+</script>
+
+<template>
+  <header class="flex justify-between items-center px-4 py-2 border-b border-gray-200">
+    <div class="flex flex-col">
+      <p class="text-lg font-semibold">Event Calendar</p>
+      <p class="text-sm text-gray-400">by <span class="font-semibold">Anton Zavalniuk</span></p>
+    </div>
+
+    <div class="flex items-center gap-2">
+      <Button variant="outline" size="icon-lg">
+        <Github class="h-[1.2rem] w-[1.2rem]"/>
+        <span class="sr-only">GitHub Link</span>
+      </Button>
+      <ThemeToggle />
+    </div>
+  </header>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Calendar from '../components/calendar/AppCalendar.vue'
+import AppHeader from '../components/shared/AppHeader.vue'
 </script>
 
 <template>
   <main>
+    <AppHeader />
     <Calendar />
   </main>
 </template>
+``
